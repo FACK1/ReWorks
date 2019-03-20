1b922c6c233fa6a568d8c2c168d309af67bb04ca
amanshawar <amanshawar_a@hotmail.com>
2019-03-20 14:04:25 +0200

    set up server #23

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const db = require('./database/dbConfig/dbConnection.js');
+const app = require('./app');
+require('env2')('config.env');
+
+app.set('PORT', process.env.PORT);
+
+db.on('connected', () => {
+  app.listen(app.get('PORT'), () => {
+    // eslint-disable-next-line no-console
+    console.log(`The Server Is Up And Running On http://localhost:${app.get('PORT')}`);
+  });
+});
