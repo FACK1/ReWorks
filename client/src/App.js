d95205399f9956315c0c1a3333d45fe1a9808967
amanshawar <amanshawar_a@hotmail.com>
2019-03-20 11:35:04 +0200

    fix code after eslint #25

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Hello</h1>
-    );
-  }
+function App() {
+  return (
+    <h1> Hello </h1>
+  );
 }
 
 export default App;
