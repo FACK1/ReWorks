977e8b013b467f91236e3bd0537dbf7469520d53
fdo2 <fdo2@live.com>
2019-03-21 14:44:32 +0200

    connect upload component with App.js #34 #1

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import GetDetails from './components/GetDetails';
+import Upload from "./components/Upload";
 
 function App() {
   return (
     <Router>
       <Route path="/GetDetails" component={GetDetails} />
+      <Route path="/upload-photo" component={Upload} />
     </Router>
+
   );
 }
 
