871d9c12c7ab7bfe52fb94b12a8d406c7ebb6e98
orjwan98 <orjwan98@hotmail.com>
2019-03-21 14:21:57 +0200

    cleaned up a bit

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -1,30 +1,27 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import axios from 'axios';
 import Header from '../Shared/Header';
 import Footer from '../Shared/Footer';
 
-class FileUpload extends Component {
-
-  handleUploadFile = (event) => {
+class Upload extends Component {
+  handleUploadFile = event => {
     const data = new FormData();
     data.append('file', event.target.files[0]);
-    axios.post('/add-to-amazon', data).then((response) => {
+    axios.post('/add-to-amazon', data).then(response => {
       console.log(response);
     });
-  }
+  };
 
-  render () {
+  render() {
     return (
-<React.Fragment>
-  <Header/>
-  <form onSubmit={this.submitFile}>
-    <input label='upload file' type='file' onChange={this.handleUploadFile} />
-    <br/>
-    <img id="logo" src={this.state.loc} alt="Smiley face" height="300" width="300"/>
-  </form>
-  <Footer/>
-<React.Fragment/>
+      <React.Fragment>
+        <Header />
+        <form>
+          <input label="upload file" type="file" onChange={this.handleUploadFile} />
+        </form>
+        <Footer />
+      </React.Fragment>
     );
   }
 }
