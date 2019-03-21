2a978ec3e3571686d5b5e39ccf9ce33ed41fae7f
orjwan98 <orjwan98@hotmail.com>
2019-03-21 14:21:16 +0200

    add a react route for /upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import GetDetails from './components/GetDetails';
+import Upload from './components/Upload/index.js';
 
 function App() {
   return (
     <Router>
       <Route path="/GetDetails" component={GetDetails} />
+      <Route path="/upload" component={Upload} />
     </Router>
   );
 }
-
 
 export default App;
