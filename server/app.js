f697a7e4a6223ba5ae007d882b3a7d1a2756d6ee
orjwan98 <orjwan98@hotmail.com>
2019-03-21 10:21:09 +0200

    require router & use it on all end points. serve up the build on any end point requested from front

--- server/app.js
+++ server/app.js
@@ -1,7 +1,15 @@
-const express = require('express');
 const path = require('path');
-require('env2')('config.env');
+const express = require('express');
 
 const app = express();
+require('env2')('config.env');
+
+const router = require('./router');
+
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+app.use(router);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
 module.exports = app;
