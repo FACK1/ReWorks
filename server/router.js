fe05fabe5cff8bb058e85eb37d0eed69c7bcf1c7
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-03-24 11:57:03 +0200

    add /login and /logout routes to router

--- server/router.js
+++ server/router.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const uploadPhoto = require('./controllers/uploadPhoto');
+const { login, logout } = require('./controllers/login.js');
 
 router.post('/add-to-amazon', uploadPhoto);
+router.post('/login', login);
+router.get('/logout', logout);
 
 module.exports = router;
