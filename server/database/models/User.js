e0c091791cabcb9c08cc8476f018daf30a6c0fa1
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-03-24 11:49:35 +0200

    add userAirtableId field to User model related #44

--- server/database/models/User.js
+++ server/database/models/User.js
@@ -1,9 +1,9 @@
-
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
   username: String,
   password: { type: String, required: false },
+  userairtableid: { type: String, required: false },
 });
 
 module.exports = mongoose.model('User', UserSchema);
