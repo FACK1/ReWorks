bc5fe728568d8a4c0e0804fdec8e9caf276a3e71
amanshawar <amanshawar_a@hotmail.com>
2019-03-24 09:44:07 +0200

    make style for get details page  #34

--- client/src/components/GetDetails/index.js
+++ client/src/components/GetDetails/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Title from '../Shared/Title';
 import Header from '../Shared/Header';
+import Form from '../Shared/Form';
+import GButton from '../Shared/GreenButton';
+import Button from '../Shared/Button';
 
 function GetDetails() {
   return (
     <React.Fragment>
       <Title />
       <Header />
+      <Form />
+      <Button />
+      <GButton />
     </React.Fragment>
   );
 }
