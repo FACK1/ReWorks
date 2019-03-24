c0074df7189ed4d964947ac3890abf7aadbab0b9
orjwan98 <orjwan98@hotmail.com>
2019-03-24 08:52:11 +0200

    import Upload to App and set route

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import GetDetails from './components/GetDetails';
-import Upload from './components/Upload/index.js';
+import Upload from './components/Upload';
 
 function App() {
   return (
     <Router>
-      <Route path="/GetDetails" component={GetDetails} />
-      <Route path="/upload" component={Upload} />
+      <div className="App">
+        <Route path="/GetDetails" component={GetDetails} />
+        <Route path="/upload" component={Upload} />
+      </div>
     </Router>
   );
 }
