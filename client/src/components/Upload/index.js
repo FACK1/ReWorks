c9b915f95e2f89f6de5745e9085af31e2572b01b
orjwan98 <orjwan98@hotmail.com>
2019-03-25 15:27:49 +0200

    surround the uploadPhoto icon with a label with a for attr that points to the input id

--- client/src/components/Upload/index.js
+++ client/src/components/Upload/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Header from '../Shared/Header';
 import Title from '../Shared/Title';
+import uploadIcon from './uploadIcon.png';
 
 class Upload extends Component {
   state = {
@@ -21,7 +22,16 @@ class Upload extends Component {
         <Title />
         <Header />
         <form>
-          <input label="upload file" type="file" onChange={this.handleUploadFile} />
+          <input
+            id="input-img"
+            label="upload file"
+            type="file"
+            onChange={this.handleUploadFile}
+            hidden
+          />
+          <label for="input-img">
+            <img src={uploadIcon} />
+          </label>
           <br />
           <img id="logo" src={this.state.loc} alt="uploaded photo" height="300" width="300" />
         </form>
