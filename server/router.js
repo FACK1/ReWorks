2db9ea903296327cc6ca1152e0bd15cc54c8c7ca
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-03-25 12:46:12 +0200

    call the S3_uploadPhoto middleware through /add-to-amazon endpoint and call clarifai apis to get photo details

--- server/router.js
+++ server/router.js
@@ -2,12 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const uploadPhoto = require('./controllers/uploadPhoto');
+const { uploadPhoto } = require('./middlewares/S3_uploadPhoto');
 const { login, logout } = require('./controllers/login.js');
 const { addItem } = require('./controllers/addItem.js');
 const { auth } = require('./middlewares/auth.js');
+const { clarifaiAPIs } = require('./controllers/clarifaiAPIs');
 
-router.post('/add-to-amazon', uploadPhoto);
+router.post('/add-to-amazon', uploadPhoto, clarifaiAPIs);
 router.post('/login', login);
 router.get('/logout', logout);
 router.post('/add-item', auth, addItem);
