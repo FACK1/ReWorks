edf0b61b9266ebe44a5b81083f77f24939a12831
orjwan98 <orjwan98@hotmail.com>
2019-03-25 14:14:49 +0200

    Add exact to Routes. Add the Splash component to render

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import GetDetails from './components/GetDetails';
 import Upload from './components/Upload';
+import Splash from './components/SplashPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/get-details" component={GetDetails} />
-        <Route path="/upload-photo" component={Upload} />
+        <Route exact path="/get-details" component={GetDetails} />
+        <Route exact path="/upload-photo" component={Upload} />
+        <Route exact path="/" component={Splash} />
       </div>
     </Router>
   );
