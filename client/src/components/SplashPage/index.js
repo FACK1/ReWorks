1c1ea61e94296d7659194e54eefea6b370d84782
orjwan98 <orjwan98@hotmail.com>
2019-03-26 12:09:00 +0200

    build Splash styled components according to Figma design

--- client/src/components/SplashPage/index.js
+++ client/src/components/SplashPage/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import Title from '../Shared/Title';
 import GButton from '../Shared/GreenButton';
-import Footer from '../Shared/Footer';
-import { AppDescription } from './splashpage.style';
+import { AppDescription, FatherContainer,BContainer, Span } from './splashpage.style';
 
 const Splash = () => (
-  <React.Fragment>
+<React.Fragment>
+  <FatherContainer>
     <Title />
-    <AppDescription>
-      Ecomit makes it easy for you to trade your preloved items online. Simply upload images of your
-      items & allow our app to generate the data needed to trade on.
-    </AppDescription>
-    <AppDescription>Start by uploading a photo of your item</AppDescription>
-    <GButton action="START" center />
-    <Footer />
-  </React.Fragment>
+      <AppDescription>
+        Ecommit makes it easy for you to trade your preloved items online. Simply upload images of your
+        items & allow our app to generate the data needed to trade on.
+
+<Span>Start by uploading a photo of your item</Span>
+</AppDescription>
+      <BContainer>
+        <GButton action="START"/>
+      </BContainer>
+  </FatherContainer>
+</React.Fragment>
 );
 
 export default Splash;
