a8de1624de6e92f95faef6df7db8a5c01ca3700a
orjwan98 <orjwan98@hotmail.com>
2019-03-26 12:07:34 +0200

    merged with master

--- client/src/components/Shared/GreenButton/index.js
+++ client/src/components/Shared/GreenButton/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import StyledButton from './greenbutton.style';
 
-<<<<<<< HEAD
 const GButton = ({ action }) => <StyledButton type="button">{action}</StyledButton>;
-=======
-const GButton = () => (<StyledButton type="button">CONTINUE</StyledButton>
-);
->>>>>>> master
+
 export default GButton;
