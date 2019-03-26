56fdc9453e33b4ca52b8aab6337c4218cc2161b1
orjwan98 <orjwan98@hotmail.com>
2019-03-26 12:09:52 +0200

    made styled components for the Splash page

--- client/src/components/SplashPage/splashpage.style.js
+++ client/src/components/SplashPage/splashpage.style.js
@@ -1,6 +1,37 @@
 import styled from 'styled-components';
 
+
+
+export const FatherContainer = styled.div`
+text-align: center;
+height:100vh;
+display: flex;
+flex-flow: column wrap;
+align-items: center;
+color:white;
+background:  linear-gradient(#3CBA92, #1F604B)
+`;
+
 export const AppDescription = styled.p`
-  font-family: Assistant;
-  text-align: center;
+font-family: Assistant;
+padding: 0px 40px 0 40px;
+line-height: 2;
+font-size:24px;
+@media (max-width: 650px) {
+  font-size: 20px;
+}
+
+`;
+
+
+export const BContainer = styled.div`
+margin: auto;
+width:100%;
+height:30%;
+`;
+
+
+export const Span = styled.span`
+margin:20px 0px 0px 20px;
+display:block;
 `;
