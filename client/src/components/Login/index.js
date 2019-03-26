f986c425e99566ad5bc9b411676d1d44c106b529
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-03-26 14:45:24 +0200

    set inputs value in state

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -18,9 +18,20 @@ import {
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      inputValue: '',
+    };
   }
 
+  updateInputValue = (event) => {
+    this.setState({
+      inputValue: event.target.value,
+    });
+    console.log(this.state.inputValue);
+  };
+
   render() {
+    console.log(this.state.inputValue);
     return (
       <React.Fragment>
         <Title />
@@ -29,9 +40,21 @@ class Login extends Component {
 
         <StyledForm>
           <StyledLabel> Username* </StyledLabel>
-          <StyledInput type="text" name="username" placeholder="username" />
+          <StyledInput
+            type="text"
+            name="username"
+            placeholder="username"
+            value={this.state.inputValue}
+            onChange={this.updateInputValue}
+          />
           <StyledLabel> Password* </StyledLabel>
-          <StyledInput type="password" name="password" placeholder="password" />
+          <StyledInput
+            type="password"
+            name="password"
+            placeholder="password"
+            value={this.state.inputValue}
+            onChange={this.updateInputValue}
+          />
 
           <StyledP>
             <StyledText>First time around here? </StyledText>
