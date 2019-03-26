9d4b9c6d0d860effaa8ba0a8f97bf777a2d9eda6
amanshawar <amanshawar_a@hotmail.com>
2019-03-26 15:44:45 +0200

    get the items for user BE #64

---
+++ server/controllers/getItems.js
@@ -0,0 +1,30 @@
+const Airtable = require('airtable');
+
+const { Airtable_API_KEY } = process.env;
+const base = new Airtable({ apiKey: Airtable_API_KEY }).base('appAZnpLnWP0wjAc6');
+
+exports.getItems = (req, res) => {
+  const { id } = req.params;
+  const data = [];
+
+  base('Items').select({
+    view: 'Main View',
+  }).eachPage((records, fetchNextPage) => {
+    records.forEach((record) => {
+      if (record.get('Users')[0] === id) {
+        data.push({
+          type: record.get('Type'),
+          size: record.get('Size'),
+          url: record.get('Image URL'),
+          img: record.get('Image')[0].url,
+        });
+      }
+    });
+    fetchNextPage();
+  }, (err) => {
+    if (err) {
+      return res.json({ success: 'false', message: err });
+    }
+    return res.json({ success: 'true', data });
+  });
+};
