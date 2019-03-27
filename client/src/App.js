59910090ae31a442f9d153596e2330e506909fe8
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-03-27 06:58:35 +0200

    add path for ItemDetails component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import GetDetails from './components/GetDetails';
 import Upload from './components/Upload';
 import Login from './components/Login';
+import ItemDetails from './components/ItemDetails';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/get-details" component={GetDetails} />
         <Route path="/upload-photo" component={Upload} />
         <Route path="/login" component={Login} />
+        <Route path="/item-details" component={ItemDetails} />
       </div>
     </Router>
   );
