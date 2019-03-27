6bb07229f77f6e483c0fe6162b143974b8048010
amanshawar <amanshawar_a@hotmail.com>
2019-03-27 14:20:24 +0200

    make the sign up BE #70

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -1,31 +1,41 @@
 const bcrypt = require('bcryptjs');
+const Airtable = require('airtable');
 const jwt = require('jsonwebtoken');
 
-const User = require('../database/models/User');
+const { Airtable_API_KEY } = process.env;
+const base = new Airtable({ apiKey: Airtable_API_KEY }).base('appAZnpLnWP0wjAc6');
 
-const { SECRET } = process.env;
+const User = require('../database/models/User');
 
 
 exports.signUp = (req, res) => {
   const { username, password, confirmPassword } = req.body;
-  console.log(username, password, confirmPassword);
   User.findOne({ username })
     .then((user) => {
       if (user) {
         return res.json({ error: 'User exist , please use another one' });
       }
       if (password === confirmPassword) {
-        bcrypt.hash(password, 10)
-          .then(hashedPassword => new User({
-            username,
-            password: hashedPassword,
-          })).then((addUser) => {
-            addUser.save((err, result) => {
-              if (err) return res.json({ error: err });
-              res.json({ result, message: 'added to database' });
+        base('Users').create({
+          Name: username,
+        }, (err, record) => {
+          if (err) {
+            return res.json({ error: err });
+          }
+
+          bcrypt.hash(password, 10)
+            .then(hashedPassword => new User({
+              username,
+              password: hashedPassword,
+              userairtableid: record.getId(),
+            })).then((addUser) => {
+              addUser.save((err2, result) => {
+                if (err) return res.json({ error: err2 });
+                return res.json({ result, message: 'added to database & airtable' });
               // res.redirect('/login');
-            });
-          }).catch((err) => { res.json({ error: err }); });
+              });
+            }).catch((err3) => { res.json({ error: err3 }); });
+        });
       } else {
         return res.json({ error: 'the password and confirm password not match' });
       }
