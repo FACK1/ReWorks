3a0c165a830c9067d6b63e0b185005e2bb0b3b7e
amanshawar <amanshawar_a@hotmail.com>
2019-03-27 14:59:35 +0200

    make front end sgin uo

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import GetDetails from './components/GetDetails';
 import Upload from './components/Upload';
 import Splash from './components/SplashPage';
 import Login from './components/Login';
+import SignUp from './components/SignUp';
 import ItemDetails from './components/ItemDetails';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/upload-photo" component={Upload} />
         <Route exact path="/" render={props => <Splash {...props} />} />
         <Route path="/login-form" component={Login} />
+        <Route path="/signup-form" component={SignUp} />
         <Route path="/item-details" component={ItemDetails} />
       </div>
     </Router>
