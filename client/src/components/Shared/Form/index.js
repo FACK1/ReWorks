9dfe316e90786ff11988aaf89a3b8a2994c326c6
orjwan98 <orjwan98@hotmail.com>
2019-03-28 12:04:55 +0200

    for details look at comments regarding src/components/Shared/Form/index.js file

--- client/src/components/Shared/Form/index.js
+++ client/src/components/Shared/Form/index.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ModalProvider } from "styled-react-modal";
+import Popup from "../Popup";
 
 import {
   StyledForm,
@@ -13,14 +15,79 @@ import {
   StyledLabels,
   StyledInput,
   StyledTextarea,
-  StyledImgCon,
-}
-  from './form.style';
+  StyledImgCon
+} from "./form.style";
 
 class Form extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isOpen: false,
+      selected: null,
+      categories: [
+        "Item Type*",
+        "Color*",
+        "Brand",
+        "Other",
+        "Condition",
+        "Label size",
+        "Age",
+        "purchase price"
+      ],
+      colors: ["white", "black", "red", "blue", "green", "limegreen", "gray"],
+      brands: [
+        "cool",
+        "cooler",
+        "coolest",
+        "the coolest",
+        "THE coolest",
+        "THE COOLEST",
+        "THE ABSOLUTE COOLEST"
+      ],
+      itemType: [
+        "shirt",
+        "trousers",
+        "leggings",
+        "yoga pants",
+        "button-down",
+        "hat",
+        "beanie",
+        "coat"
+      ],
+      conditions: [
+        "new",
+        "worn once",
+        "worn less than five times",
+        "not worn at all",
+        "semi-new",
+        "not new at all",
+        "just... no",
+        "totally nah"
+      ],
+      labelSizes: ["XS", "S", "M", "L", "XL", "XXL", "XXXL", "XXXXL", "XXXXXL"],
+      ages: [
+        "one year",
+        "6 months",
+        "3 months",
+        "1 months",
+        "2 weeks",
+        "4 weeks",
+        "9 weeks"
+      ]
+    };
   }
+
+  toggleOpen = e => {
+    const clicked = e.target.value.split(".");
+    if (clicked[0] === "more") {
+      this.setState({ isOpen: true, selected: clicked[1] });
+    }
+  };
+
+  toggleClose = e => {
+    e.preventDefault();
+    this.setState({ isOpen: false });
+  };
 
   render() {
     const { image } = this.props;
@@ -33,58 +100,97 @@ class Form extends Component {
         <StyledInformation>PIECE INFORMATION</StyledInformation>
         <StyledNotic>Please verify the items marked* </StyledNotic>
 
-
         <StyledDiv>
-
           <StyledLabels>
-
-            <StyledLabel> Item Type* </StyledLabel>
-            <StyledLabel> Color* </StyledLabel>
-            <StyledLabel> Brand </StyledLabel>
-            <StyledLabel> Other </StyledLabel>
-            <StyledLabel> Condition </StyledLabel>
-            <StyledLabel> Label size </StyledLabel>
-            <StyledLabel> Age </StyledLabel>
-            <StyledLabel> purchase price </StyledLabel>
-
+            {this.state.categories.map(category => {
+              return <StyledLabel key={category}>{category}</StyledLabel>;
+            })}
           </StyledLabels>
 
           <StyledItem>
-
-            <StyledSelect>
-              <StyledOption value="T-shirt">T-shirt</StyledOption>
+            <StyledSelect onChange={this.toggleOpen}>
+              {this.state.itemType.slice(0, 5).map(item => {
+                return (
+                  <StyledOption key={item} value={item}>
+                    {item}
+                  </StyledOption>
+                );
+              })}
+              <StyledOption value="more.itemType">More...</StyledOption>
             </StyledSelect>
 
-            <StyledSelect>
-              <StyledOption value="white">white</StyledOption>
+            <StyledSelect onChange={this.toggleOpen}>
+              {this.state.colors.slice(0, 5).map(color => {
+                return (
+                  <StyledOption key={color} value={color}>
+                    {color}
+                  </StyledOption>
+                );
+              })}
+              <StyledOption value="more.colors">More...</StyledOption>
             </StyledSelect>
 
-            <StyledSelect>
-              <StyledOption value="Nike">Nike</StyledOption>
+            <StyledSelect onChange={this.toggleOpen}>
+              {this.state.brands.slice(0, 5).map(brand => {
+                return (
+                  <StyledOption key={brand} value={brand}>
+                    {brand}
+                  </StyledOption>
+                );
+              })}
+              <StyledOption value="more.brands">More...</StyledOption>
             </StyledSelect>
 
             <StyledInput type="text" name="brand" placeholder="brand" />
 
-            <StyledSelect>
-              <StyledOption value="Nike">Nike</StyledOption>
+            <StyledSelect onChange={this.toggleOpen}>
+              {this.state.conditions.slice(0, 5).map(condition => {
+                return (
+                  <StyledOption key={condition} value={condition}>
+                    {condition}
+                  </StyledOption>
+                );
+              })}
+              <StyledOption value="more.conditions">More...</StyledOption>
             </StyledSelect>
 
-            <StyledSelect>
-              <StyledOption value="Nike">Nike</StyledOption>
+            <StyledSelect onChange={this.toggleOpen}>
+              {this.state.labelSizes.slice(0, 5).map(size => {
+                return (
+                  <StyledOption key={size} value={size}>
+                    {size}
+                  </StyledOption>
+                );
+              })}
+              <StyledOption value="more.labelSizes">More...</StyledOption>
             </StyledSelect>
 
-            <StyledSelect>
-              <StyledOption value="Nike">Nike</StyledOption>
+            <StyledSelect onChange={this.toggleOpen}>
+              {this.state.ages.slice(0, 5).map(age => {
+                return (
+                  <StyledOption key={age} value={age}>
+                    {age}
+                  </StyledOption>
+                );
+              })}
+              <StyledOption value="more.ages">More...</StyledOption>
             </StyledSelect>
 
             <StyledInput type="text" name="price" placeholder="price" />
-
           </StyledItem>
-
         </StyledDiv>
 
-        <StyledTextarea name="extra" placeholder="More  . e.g. What do you love about it?" />
-
+        <StyledTextarea
+          name="extra"
+          placeholder="More .e.g. What do you love about it?"
+        />
+        <ModalProvider>
+          <Popup
+            open={this.state.isOpen}
+            data={this.state[this.state.selected]}
+            toggleClose={this.toggleClose}
+          />
+        </ModalProvider>
       </StyledForm>
     );
   }
