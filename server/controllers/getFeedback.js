f133086f525e3cc6233492d75a74e7df43b94185
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-03-31 09:31:54 +0300

    rename error variable with err

--- server/controllers/getFeedback.js
+++ server/controllers/getFeedback.js
@@ -6,9 +6,9 @@ const base = new Airtable({ apiKey: Airtable_API_KEY }).base('appAZnpLnWP0wjAc6'
 exports.getFeedback = (req, res) => {
   const { userAirtableId } = req;
 
-  base('Users').find(userAirtableId, (error, record) => {
-    if (error) {
-      return res.status(500).json({ error });
+  base('Users').find(userAirtableId, (err, record) => {
+    if (err) {
+      return res.status(500).json({ err });
     }
     const feedback = record.get('Feedback');
     return res.status(200).json({ success: true, feedback });
