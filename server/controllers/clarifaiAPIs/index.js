e0719377d47ed6c9e87a022a30dd9ac6fa61cf41
orjwan98 <orjwan98@hotmail.com>
2019-04-01 14:14:28 +0300

    removed console logs

--- server/controllers/clarifaiAPIs/index.js
+++ server/controllers/clarifaiAPIs/index.js
@@ -1,25 +1,20 @@
-const apparelAPI = require("./apparelAPI");
-const colorAPI = require("./colorAPI");
-const generalAPI = require("./generalAPI");
+const apparelAPI = require('./apparelAPI');
+const colorAPI = require('./colorAPI');
+const generalAPI = require('./generalAPI');
 
 exports.clarifaiAPIs = (req, res) => {
   const { image_url } = req;
   Promise.all([
     apparelAPI(image_url),
     colorAPI(image_url),
-    generalAPI(image_url)
-  ]).then(values => {
+    generalAPI(image_url),
+  ]).then((values) => {
     const data = {
       image_url,
       apparel: values[0],
       colors: values[1],
-      general: values[2]
+      general: values[2],
     };
-    console.log("START HERE");
-    console.log(data.apparel.data);
-    console.log(data.colors.data);
-    console.log(data.general.data);
-    console.log("END HERE");
 
     return res.json(data);
   });
