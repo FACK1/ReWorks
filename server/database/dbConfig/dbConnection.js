2c1a7b1c8aece30c4f6ecbebef915c74b9c5cba9
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-04-01 15:00:10 +0300

    check if DB_URL exist if not throw error related #106

--- server/database/dbConfig/dbConnection.js
+++ server/database/dbConfig/dbConnection.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose');
 require('env2')('config.env');
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useCreateIndex: true });
+if (!process.env.DB_URL) {
+  throw new Error('Missing Mongodb DB_URL env var');
+}
+
+mongoose.connect(
+  process.env.DB_URL,
+  { useNewUrlParser: true, useCreateIndex: true },
+);
 
 const db = mongoose.connection;
 // eslint-disable-next-line no-console
-db.on('error', () => { console.log('wrong connection;   !'); });
+db.on('error', () => {
+  console.log('wrong connection;   !');
+});
 // eslint-disable-next-line no-console
-db.once('open', () => { console.log('mongoose connected successfully!'); });
+db.once('open', () => {
+  console.log('mongoose connected successfully!');
+});
 
 module.exports = db;
