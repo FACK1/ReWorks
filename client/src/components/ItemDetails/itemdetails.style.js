3d7fb51fe9ead02ddae154d08c99a5b848fc2631
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-04-01 10:19:03 +0300

    style the button

--- client/src/components/ItemDetails/itemdetails.style.js
+++ client/src/components/ItemDetails/itemdetails.style.js
@@ -4,7 +4,11 @@ export const ImgDiv = styled.div`
   position: absolute;
   display: flex;
   justify-content: flex-end;
-  z-index: -1;
   top: 270px;
   width: 90%;
+`;
+
+export const DeleteButton = styled.button`
+  background-color: white;
+  border: none;
 `;
