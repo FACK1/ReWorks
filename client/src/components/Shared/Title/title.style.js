c114b2460eab15d7546e03d717e88fd90c298a08
amanshawar <amanshawar_a@hotmail.com>
2019-04-01 10:32:04 +0300

    indintation #27

--- client/src/components/Shared/Title/title.style.js
+++ client/src/components/Shared/Title/title.style.js
@@ -1,24 +1,24 @@
 import styled from 'styled-components';
 
 export const StyledTitle = styled.h1`
-font-size: 34px;
-font-family:'Kaushan Script';
-text-align:center;
-font-weight: normal;
+  font-size: 34px;
+  font-family:'Kaushan Script';
+  text-align:center;
+  font-weight: normal;
 
-@media (max-width: 650px) {
-  font-size: 30px;
-}
+  @media (max-width: 650px) {
+    font-size: 30px;
+  }
 `;
 
 export const LogoutB = styled.h1`
-font-size: 16px;
-position:absolute;
-top:30px;
-right:30px;
-font-weight: normal;
+  font-size: 16px;
+  position:absolute;
+  top:30px;
+  right:30px;
+  font-weight: normal;
 
-@media (max-width: 650px) {
-  font-size: 12px;
-}
+  @media (max-width: 650px) {
+    font-size: 12px;
+  }
 `;
