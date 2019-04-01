0ebda452ce3f4bf64670c259d48ad6197e318806
orjwan98 <orjwan98@hotmail.com>
2019-04-01 12:03:04 +0300

    turned popup component into a functional component cuz there's no state

--- client/src/components/Shared/Popup/index.js
+++ client/src/components/Shared/Popup/index.js
@@ -1,41 +1,29 @@
-import React, { Component } from "react";
-import { StyledModal, Category, CloseBtn, DataContainer } from "./popup.style";
+import React, { Component } from 'react';
+import {
+  StyledModal, Category, CloseBtn, DataContainer,
+} from './popup.style';
 
-class Popup extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { open, toggleClose, data, changeSelected, name } = this.props;
+const Popup = ({
+  open, toggleClose, data, changeSelected, name,
+}) => {
+  {
     const dataHere = data
-      ? data.slice(5).map(ele => {
-          return (
-            <CloseBtn
-              key={ele}
-              type="submit"
-              value={name + "." + ele}
-              onClick={changeSelected}
-            >
-              {ele}
-            </CloseBtn>
-          );
-        })
+      ? data.slice(5).map(ele => (
+        <CloseBtn key={ele} type="submit" value={`${name}.${ele}`} onClick={changeSelected}>
+          {ele}
+        </CloseBtn>
+      ))
       : null;
     return (
       <React.Fragment>
-        <StyledModal
-          isOpen={open}
-          onBackgroundClick={toggleClose}
-          onEscapeKeydown={toggleClose}
-        >
+        <StyledModal isOpen={open} onBackgroundClick={toggleClose} onEscapeKeydown={toggleClose}>
           <CloseBtn type="submit" onClick={toggleClose}>
             close
           </CloseBtn>
           <Category>
             {`${name}`
               .split(/(?=[A-Z])/)
-              .join(" ")
+              .join(' ')
               .toUpperCase()}
           </Category>
           <DataContainer>{dataHere}</DataContainer>
@@ -43,6 +31,6 @@ class Popup extends Component {
       </React.Fragment>
     );
   }
-}
+};
 
 export default Popup;
