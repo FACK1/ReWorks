f561ff07e87f2759bbee51651772bdc4ed564d1a
orjwan98 <orjwan98@hotmail.com>
2019-04-02 10:26:07 +0300

    moved handlers and state from Form to GetDetails component and passed them to form as props

--- client/src/components/GetDetails/index.js
+++ client/src/components/GetDetails/index.js
@@ -8,6 +8,17 @@ import Button from '../Shared/Button';
 import Footer from '../Shared/Footer';
 
 class GetDetails extends Component {
+  state={
+    isOpen: false,
+    selectedCat: null,
+    selected_itemType: '',
+    selected_colors: '',
+    selected_brands: '',
+    selected_condition: '',
+    selected_labelSize: '',
+    selected_age: '',
+  }
+
   continue = () => {
     axios.get('/checkcookie').then(({ data: { cookie, logged } }) => {
       const { history } = this.props;
@@ -19,13 +30,49 @@ class GetDetails extends Component {
     });
   };
 
+  toggleOpen = (e) => {
+    const clicked = e.target.value.split('.');
+    if (clicked[1] === 'more') {
+      this.setState({ isOpen: true, selectedCat: clicked[0] });
+    } else {
+      const selected = `selected_${clicked[0]}`;
+      this.setState({ [selected]: e.target.value });
+    }
+  };
+
+  toggleClose = (e) => {
+    e.preventDefault();
+    this.setState({ isOpen: false });
+  };
+
+  changeSelected = (e) => {
+    e.preventDefault();
+    const selected = `selected_${this.state.selectedCat}`;
+    this.setState({ [selected]: e.target.value.split('.')[1], isOpen: false });
+  };
+
   render() {
     const { image_url, apparel, colors } = this.props.location.details;
     return (
       <React.Fragment>
         <Title {...this.props} />
         <Header title="Get your details" />
-        <Form image={image_url} apparel={apparel} colors={colors} />
+        <Form
+          image={image_url}
+          apparel={apparel}
+          colors={colors}
+          isOpen={this.state.isOpen}
+          selected_itemType={this.state.selected_itemType}
+          selected_colors={this.state.selected_colors}
+          selected_brands={this.state.selected_brands}
+          selected_condition={this.state.selected_condition}
+          selected_labelSize={this.state.selected_labelSize}
+          selected_age={this.state.selected_age}
+          selectedCat={this.state.selectedCat}
+          toggleOpen={this.toggleOpen}
+          toggleClose={this.toggleClose}
+          changeSelected={this.changeSelected}
+        />
         <Button />
         <GButton title="CONTINUE" onClick={this.continue} />
         <Footer />
