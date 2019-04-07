385645fa61bb76bc73357a774121d267c2d8656d
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-04-07 15:21:00 +0300

    call /getbrands endpoint and save data in state related #159

--- client/src/components/ItemDetails/index.js
+++ client/src/components/ItemDetails/index.js
@@ -7,7 +7,7 @@ import Button from '../Shared/Button';
 import Footer from '../Shared/Footer';
 import deleteIcon from './garbage.png';
 import {
-  brands, itemType, condition, labelSize, age,
+  itemType, condition, labelSize, age,
 } from '../../data';
 
 import { ImgDiv, DeleteButton } from './itemdetails.style';
@@ -19,8 +19,8 @@ class ItemDetails extends Component {
     selectedCat: null,
     selected_brands: { id: '', brandName: '', name: '' },
     itemType,
-    colors: ['Black'],
-    brands,
+    colors: [],
+    brands: [],
     condition,
     labelSize,
     age,
@@ -44,6 +44,13 @@ class ItemDetails extends Component {
       selected_price: itemDetails.price,
       selected_details: itemDetails.details,
       colors: clarifaiColors,
+    });
+
+    axios.get('/getbrands').then(({ data }) => {
+      if (data.success) {
+        const brands = data.data;
+        this.setState({ brands });
+      }
     });
   }
 
