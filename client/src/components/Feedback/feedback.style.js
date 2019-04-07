742605ace7da2b6b512879c032c933b1f1305ade
fdo2 <fdo2@live.com>
2019-04-07 09:44:44 +0300

    edit style for feedback page and add style for the added text #152

--- client/src/components/Feedback/feedback.style.js
+++ client/src/components/Feedback/feedback.style.js
@@ -2,14 +2,19 @@ import styled from 'styled-components';
 
 export const ThanksText = styled.p`
   font-weight: bold;
-  font-size: 19.93px;
+  font-size: 17.5px;
   text-align: center;
+  margin-left: 30px;
+  margin-right: 30px;
+  margin-top: 15px;
+  margin-bottom: 20px;
 `;
 
 export const CheckboxContainer = styled.div`
   margin-top: 30px;
   display: flex;
   flex-flow: column wrap;
+  font-size: 25px;
 `;
 
 export const BottomContainer = styled.div`
@@ -17,4 +22,18 @@ export const BottomContainer = styled.div`
   right: 0;
   bottom: 0;
   left: 0;
+`;
+
+export const Para = styled.p`
+  margin-top: 30px;
+  display: flex;
+  text-align: center;
+  flex-flow: column wrap;
+  font-size: 16px;
+  font-weight: bold;
+  padding-top:1px;
+  margin-top: 15px;
+  margin-bottom: 15px;
+  margin-left: 30px;
+  margin-right: 30px;
 `;
