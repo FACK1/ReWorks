6c0ff9e676c4106e1a4c56cd86d935687f446d10
fdo2 <fdo2@live.com>
2019-04-07 11:44:39 +0300

    add text that Alastair asks #156

--- client/src/components/SplashPage/index.js
+++ client/src/components/SplashPage/index.js
@@ -32,9 +32,9 @@ class Splash extends Component {
         <FatherContainer>
           <Title />
           <AppDescription>
-            e-commit is a new way to sell / trade your unwanted clothes.
-            The app automatically generates information about your garments & creates a data base you can use to auto link to marketplaces or pass on (e.g. to charity).
-            Even a modest amount of data can save the earth - Add as much as you can
+            <p> e-commit is a new way to sell / trade your unwanted clothes. </p>
+            <p> The app automatically generates information about your garments & creates a data base you can use to auto link to marketplaces or pass on (e.g. to charity).</p>
+            <p> Even a modest amount of data can save the earth - Add as much as you can!</p>
           </AppDescription>
           <GButton action="START" onClick={this.clickedStart}>
         START
