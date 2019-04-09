b8d93a6c5267ce4db420f2f47a649719a7afe3a7
amanshawar <amanshawar_a@hotmail.com>
2019-04-09 10:58:29 +0300

    make the style not pushed if there alot of data #169

--- client/src/components/Item/index.js
+++ client/src/components/Item/index.js
@@ -16,31 +16,42 @@ import {
 
 const Item = ({
   color, type, imageUrl, subtitle1, subtitle2, onClick,
-}) => (
-  <ItemDiv>
-    <ItemContainer onClick={onClick}>
-      <Line />
-      <Column>
-        <ItemMainData>
-          <ItemTitle>Item:</ItemTitle>
-          <ItemText>
-            {color}
-            {' '}
-            {type}
-          </ItemText>
-        </ItemMainData>
-        <ItemTags>
-          <ItemTag>
-            <ItemSubtitle>{subtitle1}</ItemSubtitle>
-          </ItemTag>
-          <ItemTag>
-            <ItemSubtitle>{subtitle2}</ItemSubtitle>
-          </ItemTag>
-        </ItemTags>
-      </Column>
-      <ItemImage src={imageUrl} alt="your item" />
-    </ItemContainer>
-  </ItemDiv>
-);
+}) => {
+  console.log('aas');
+
+  if (color.length + type.length > 24) {
+    const l = 23 - color.length;
+    type = type.slice(0, l).concat('...');
+    console.log('aa');
+  } else {
+    console.log('a');
+  }
+  return (
+    <ItemDiv>
+      <ItemContainer onClick={onClick}>
+        <Line />
+        <Column>
+          <ItemMainData>
+            <ItemTitle>Item:</ItemTitle>
+            <ItemText>
+              {color}
+              {' '}
+              {type}
+            </ItemText>
+          </ItemMainData>
+          <ItemTags>
+            <ItemTag>
+              <ItemSubtitle>{subtitle1}</ItemSubtitle>
+            </ItemTag>
+            <ItemTag>
+              <ItemSubtitle>{subtitle2}</ItemSubtitle>
+            </ItemTag>
+          </ItemTags>
+        </Column>
+        <ItemImage src={imageUrl} alt="your item" />
+      </ItemContainer>
+    </ItemDiv>
+  );
+};
 
 export default Item;
