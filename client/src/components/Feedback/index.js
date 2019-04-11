29064809647ce0cb597f519a7635087cc3b77f0d
fdo2 <fdo2@live.com>
2019-04-11 14:54:18 +0300

    Edit feedback options #170

--- client/src/components/Feedback/index.js
+++ client/src/components/Feedback/index.js
@@ -8,17 +8,19 @@ import Footer from '../Shared/Footer';
 import Checkbox from '../Shared/Checkbox';
 import Spinner from '../Shared/Spinner';
 
-import { ThanksText, CheckboxContainer, BottomContainer, Para} from './feedback.style';
+import {
+  ThanksText, CheckboxContainer, BottomContainer, Para,
+} from './feedback.style';
 
 class Feedback extends Component {
   state = {
     options: [
-        { text: 'Donate to charity', checked: false },
-        { text: 'Sell on eBay', checked: false },
-        { text: 'Sell on another marketplace', checked: false },
-        { text: 'Swap for something else', checked: false },
-        { text: 'Repair / Refurbish / Upcycle ', checked: false },
-        { text: 'Upload to instagram', checked: false },
+      { text: 'Donate to charity', checked: false },
+      { text: 'Sell on eBay', checked: false },
+      { text: 'Sell on another marketplace', checked: false },
+      { text: 'Swap for something else', checked: false },
+      { text: 'Repair / Refurbish / Upcycle ', checked: false },
+      { text: 'Upload to instagram', checked: false },
     ],
     prevFeedback: [],
     currentFeedback: [],
