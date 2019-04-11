d9816585e64cce442ac0faa723e8d6476d45de2b
fdo2 <fdo2@live.com>
2019-04-11 15:06:32 +0300

    Delete GreenButton component copy in form component floder  #170

--- client/src/components/Shared/Form/GreenButton/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import StyledButton from './greenbutton.style';
-
-const GButton = props => (
-  <StyledButton onClick={props.onClick} type="button">
-    {props.title}
-  </StyledButton>
-);
-export default GButton;
