9d04e2bf3c8244e667606da8122af8ed83193a5d
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-04-11 15:31:49 +0300

    add select tag for size category and pass options to it ffrom get details state and send them to BE related #179

--- client/src/components/Shared/Form/index.js
+++ client/src/components/Shared/Form/index.js
@@ -26,6 +26,7 @@ class Form extends Component {
       'Brand',
       'Condition',
       'Label size',
+      'Size Category',
       'Age',
       'Purchase price',
     ],
@@ -54,6 +55,8 @@ class Form extends Component {
       labelSize,
       age,
       showDefaultOption,
+      selected_sizeCategory,
+      sizeCategory,
     } = this.props;
 
     return (
@@ -149,6 +152,24 @@ class Form extends Component {
                 </option>
               )}
               {labelSize.map(size => (labelSize.indexOf(size) >= 5 ? (
+                <StyledOption key={size} value={size} hidden>
+                  {size}
+                </StyledOption>
+              ) : (
+                <StyledOption key={size} value={size}>
+                  {size}
+                </StyledOption>
+              )))}
+              <StyledOption value="more">More...</StyledOption>
+            </StyledSelect>
+
+            <StyledSelect onChange={toggleOpen} name="sizeCategory" value={selected_sizeCategory}>
+              {showDefaultOption && (
+                <option default hidden>
+                  Select...
+                </option>
+              )}
+              {sizeCategory.map(size => (sizeCategory.indexOf(size) >= 5 ? (
                 <StyledOption key={size} value={size} hidden>
                   {size}
                 </StyledOption>
