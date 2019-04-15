a3579af41dafe4f58d7813d67c0954d91def75cb
fdo2 <fdo2@live.com>
2019-04-15 10:35:49 +0300

    Desktop desing #170

--- client/src/components/Upload/upload.style.js
+++ client/src/components/Upload/upload.style.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
 export const ImgIcon = styled.img`
+  width: 80%;
+  margin-left:7%;
   :hover {
     cursor: pointer;
+  }
+  @media (max-width: 650px) {
+    margin-left:18%;
   }
 `;
 
@@ -10,20 +15,19 @@ export const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
+  align-item: center;
   height: 30vh;
 `;
 export const Instructions = styled.div`
-  width:40%;
+  width:42%;
   display: flex;
   flex-direction: column;
-  height: 30vh;
-  margin-left:30%;
-  margin-bottom:5%;
-  margin-top: 3%;
+  height: 21vh;
+  margin-left:28%;
+  margin-bottom:7%;
   @media (max-width: 650px) {
     width:90%
-    font-size: 20px;
-    margin-bottom: 8%;
+    margin-bottom: 30%;
     margin-top: 7%;
     margin-left: 7%
   }
@@ -32,7 +36,7 @@ export const Instructions = styled.div`
 export const TakeGoodPhoto = styled.p`
   font-family: 'Assistant';
   text-align: center;
-  font-size: 24px;
+  font-size: 19px;
   padding: 0px 40px 10px 40px;
   @media (max-width: 650px) {
     font-size: 20px;
