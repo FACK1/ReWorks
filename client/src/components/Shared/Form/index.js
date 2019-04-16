a627729673b544ded215f0df9d498c52eef111e0
hadeel_salamin <hadeel_salamin@hotmail.com>
2019-04-16 09:25:32 +0300

    replacing select tag with react-select

--- client/src/components/Shared/Form/index.js
+++ client/src/components/Shared/Form/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ModalProvider } from 'styled-react-modal';
+import Select from 'react-select';
 import Popup from '../Popup';
 
 import {
@@ -35,7 +36,6 @@ class Form extends Component {
     ],
   };
 
-
   render() {
     const {
       image,
@@ -44,6 +44,7 @@ class Form extends Component {
       selectedCat,
       toggleClose,
       changeSelected,
+      handleChange,
       selected_itemType,
       selected_colors,
       selected_brands,
@@ -54,7 +55,6 @@ class Form extends Component {
       selected_details,
       selected_patterns,
       selected_currency,
-
       itemType,
       colors,
       brands,
@@ -83,164 +83,110 @@ class Form extends Component {
           </StyledLabels>
 
           <StyledItem>
-            <StyledSelect onChange={toggleOpen} name="itemType" value={selected_itemType.name}>
-              {itemType.map(item => (itemType.indexOf(item) >= 5 ? (
-                <StyledOption
-                  key={item.id}
-                  value={`{"id": "${item.id}", "name": "${item.name}"}`}
-                  hidden
-                >
-                  {item.itemType}
-                </StyledOption>
-              ) : (
-                <StyledOption
-                  key={item.id}
-                  value={`{"id": "${item.id}", "name": "${item.name}"}`}
-                >
-                  {item.itemType}
-                </StyledOption>
-              )))}
-              <StyledOption value="more">More...</StyledOption>
-            </StyledSelect>
-
-            <StyledSelect onChange={toggleOpen} name="colors" value={selected_colors}>
-              {colors.map(color => (colors.indexOf(color) >= 5 ? (
-                <StyledOption
-                  value={color.name}
-                  hexColor={color.hex}
-                  nameColor={color.name}
-                  {...this.props}
-                  hidden
-                >
-                  {color.name}
-                </StyledOption>
-              ) : (
-                <StyledOption
-                  value={color.name}
-                  hexColor={color.hex}
-                  nameColor={color.name}
-                  {...this.props}
-                >
-                  {color.name}
-                </StyledOption>
-              )))}
-              <StyledOption value="more">More...</StyledOption>
-            </StyledSelect>
-
-            <StyledSelect onChange={toggleOpen} name="patterns" value={selected_patterns}>
-              {showDefaultOption && (
-                <option default hidden>
-                  Select...
-                </option>
-              )}
-              {patterns.map(pattern => (patterns.indexOf(pattern) >= 5 ? (
-                <StyledOption key={pattern} value={pattern} hidden>
-                  {pattern}
-                </StyledOption>
-              ) : (
-                <StyledOption key={pattern} value={pattern}>
-                  {pattern}
-                </StyledOption>
-              )))}
-              <StyledOption value="more">More...</StyledOption>
-            </StyledSelect>
-
-            <StyledSelect onChange={toggleOpen} name="brands" value={selected_brands.name}>
-              {showDefaultOption && (
-              <option default hidden>
-                  Select...
-              </option>
-              )}
-              {brands.map(brand => (brands.indexOf(brand) >= 5 ? (
-                <StyledOption
-                  key={brand.name}
-                  value={`{"id": "${brand.id}", "name": "${brand.name}"}`}
-                  hidden
-                >
-                  {brand.name}
-                </StyledOption>
-              ) : (
-                <StyledOption
-                  key={brand.name}
-                  value={`{"id": "${brand.id}", "name": "${brand.name}"}`}
-                >
-                  {brand.name}
-                </StyledOption>
-              )))}
-              <StyledOption value="more">More...</StyledOption>
-            </StyledSelect>
-
-            <StyledSelect onChange={toggleOpen} name="condition" value={selected_condition}>
-              {showDefaultOption && (
-              <option default hidden>
-                  Select...
-              </option>
-              )}
-              {condition.map(cond => (condition.indexOf(cond) >= 5 ? (
-                <StyledOption key={cond} value={cond} hidden>
-                  {cond}
-                </StyledOption>
-              ) : (
-                <StyledOption key={cond} value={cond}>
-                  {cond}
-                </StyledOption>
-              )))}
-              <StyledOption value="more">More...</StyledOption>
-            </StyledSelect>
-
-            <StyledSelect onChange={toggleOpen} name="labelSize" value={selected_labelSize}>
-              {showDefaultOption && (
-              <option default hidden>
-                  Select...
-              </option>
-              )}
-              {labelSize.map(size => (labelSize.indexOf(size) >= 5 ? (
-                <StyledOption key={size} value={size} hidden>
-                  {size}
-                </StyledOption>
-              ) : (
-                <StyledOption key={size} value={size}>
-                  {size}
-                </StyledOption>
-              )))}
-              <StyledOption value="more">More...</StyledOption>
-            </StyledSelect>
-
-            <StyledSelect onChange={toggleOpen} name="sizeCategory" value={selected_sizeCategory}>
-              {showDefaultOption && (
-                <option default hidden>
-                  Select...
-                </option>
-              )}
-              {sizeCategory.map(size => (sizeCategory.indexOf(size) >= 5 ? (
-                <StyledOption key={size} value={size} hidden>
-                  {size}
-                </StyledOption>
-              ) : (
-                <StyledOption key={size} value={size}>
-                  {size}
-                </StyledOption>
-              )))}
-              <StyledOption value="more">More...</StyledOption>
-            </StyledSelect>
-
-            <StyledSelect onChange={toggleOpen} name="age" value={selected_age}>
-              {showDefaultOption && (
-              <option default hidden>
-                  Select...
-              </option>
-              )}
-              {age.map(time => (age.indexOf(time) >= 5 ? (
-                <StyledOption key={time} value={time} hidden>
-                  {time}
-                </StyledOption>
-              ) : (
-                <StyledOption key={time} value={time}>
-                  {time}
-                </StyledOption>
-              )))}
-              <StyledOption value="more">More...</StyledOption>
-            </StyledSelect>
+            <Select
+              value={selected_itemType}
+              className="basic-single"
+              classNamePrefix="select"
+              isDisabled={false}
+              isLoading={false}
+              isClearable
+              isRtl={false}
+              isSearchable
+              name="type"
+              options={types}
+              onChange={handleChange}
+            />
+            <Select
+              value={selected_brands}
+              className="basic-single"
+              classNamePrefix="select"
+              isDisabled={false}
+              isLoading={false}
+              isClearable
+              isRtl={false}
+              isSearchable
+              name="brand"
+              options={brands}
+              onChange={handleChange}
+            />
+            <Select
+              value={selected_patterns}
+              className="basic-single"
+              classNamePrefix="select"
+              isDisabled={false}
+              isLoading={false}
+              isClearable
+              isRtl={false}
+              isSearchable
+              name="brand"
+              options={brands}
+              onChange={handleChange}
+            />
+            <Select
+              value={selected_brands}
+              className="basic-single"
+              classNamePrefix="select"
+              isDisabled={false}
+              isLoading={false}
+              isClearable
+              isRtl={false}
+              isSearchable
+              name="brand"
+              options={brands}
+              onChange={handleChange}
+            />
+            <Select
+              value={selected_brands}
+              className="basic-single"
+              classNamePrefix="select"
+              isDisabled={false}
+              isLoading={false}
+              isClearable
+              isRtl={false}
+              isSearchable
+              name="brand"
+              options={brands}
+              onChange={handleChange}
+            />
+            <Select
+              value={selected_brands}
+              className="basic-single"
+              classNamePrefix="select"
+              isDisabled={false}
+              isLoading={false}
+              isClearable
+              isRtl={false}
+              isSearchable
+              name="brand"
+              options={brands}
+              onChange={handleChange}
+            />
+            <Select
+              value={selected_brands}
+              className="basic-single"
+              classNamePrefix="select"
+              isDisabled={false}
+              isLoading={false}
+              isClearable
+              isRtl={false}
+              isSearchable
+              name="brand"
+              options={brands}
+              onChange={handleChange}
+            />
+            <Select
+              value={selected_brands}
+              className="basic-single"
+              classNamePrefix="select"
+              isDisabled={false}
+              isLoading={false}
+              isClearable
+              isRtl={false}
+              isSearchable
+              name="brand"
+              options={brands}
+              onChange={handleChange}
+            />
 
             <StyledPriceContainer>
               <StyledInput
@@ -253,9 +199,9 @@ class Form extends Component {
 
               <StyledSelectCurrency onChange={toggleOpen} name="currency" value={selected_currency}>
                 {showDefaultOption && (
-                <option default hidden>
-                $
-                </option>
+                  <option default hidden>
+                    $
+                  </option>
                 )}
                 <StyledOption value="£">£</StyledOption>
                 <StyledOption value="$">$</StyledOption>
@@ -271,18 +217,17 @@ class Form extends Component {
           value={selected_details}
           placeholder="More .e.g. What do you love about it?"
         />
-        <ModalProvider>
-          <Popup
-            open={isOpen}
-            toggleClose={toggleClose}
-            changeSelected={changeSelected}
-            name={[selectedCat]}
-            data={this.props[selectedCat]}
-          />
-        </ModalProvider>
       </StyledForm>
     );
   }
 }
-
+// <ModalProvider>
+//   <Popup
+//     open={isOpen}
+//     toggleClose={toggleClose}
+//     changeSelected={changeSelected}
+//     name={[selectedCat]}
+//     data={this.props[selectedCat]}
+//   />
+// </ModalProvider>
 export default Form;
